756e5596ebe23195695072888f9f53f67fb6ca39
LorenzoCampidelli <campidellilorenzo@gmail.com>
2024-04-22 18:27:23 +0200

    Add files via upload

---
+++ src/granger.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from statsmodels.tsa.stattools import grangercausalitytests
+
+data = pd.read_csv("spoty.csv")
+
+gc_res = grangercausalitytests(data[["Gross Profit", "Premium MAUs"]], maxlag=1)
+
+print(gc_res)
+
+gc_res = grangercausalitytests(data[["Gross Profit", "Ad MAUs"]], maxlag=1)
+
+print(gc_res)
+
+gc_res = grangercausalitytests(data[["Gross Profit", "Research and Development Cost"]], maxlag=1)
+
+print(gc_res)
+
+data = pd.read_csv("all.csv")
+
+gc_res = grangercausalitytests(data[["Gross Profit", "Apk Downloads"]], maxlag=1)
+
+print(gc_res)
